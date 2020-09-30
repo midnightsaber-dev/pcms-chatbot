f542e8a425628c7dfcf507021d174cff29987d7a
SalaiChitOoLatt <salaichitoolatt1996@outlook.com>
2020-09-30 16:52:23 +0000

    create directory for event,merchant,partials,report.

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('admin/index', { title: 'Paymal' });
 });
 
 module.exports = router;
