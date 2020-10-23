f4b1a236486f6eb60e3a6918545794b83358636d
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-10-23 11:43:25 +0630

    add pg library

--- config/config.js
+++ config/config.js
@@ -1,17 +1,16 @@
-const pgp = require("pg-promise")();
-const cn = {
-  connectionString:
-    "postgres://mzhtidpzhekzgv:0d2d621c83e1736a61ec12395684097b452a29fb31a9afd154402ee083a10028@ec2-23-20-70-32.compute-1.amazonaws.com:5432/d8460q7isklitc",
-  max: 30,
-};
+const Pool = require("pg").Pool;
 
-// Creating a new database instance from the connection details:
-const db = pgp(cn);
+const cn =
+  "postgres://mzhtidpzhekzgv:0d2d621c83e1736a61ec12395684097b452a29fb31a9afd154402ee083a10028@ec2-23-20-70-32.compute-1.amazonaws.com:5432/d8460q7isklitc";
+
+const pool = new Pool({
+  connectionString: cn,
+});
 
 module.exports = {
   MAILGUN_CONFIG: {
     DOMAIN_NAME: process.env.MAILGUN_DOMAIN_NAME,
     API_KEY: process.env.MAILGUN_API_KEY,
   },
-  DATABASE: db,
+  pool: pool,
 };
