3ccedf163b8cb6037ec0474d09a2e7460d506733
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-05 02:21:52 +0630

    fix previous bug 5

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ router.post("/login", async (req, res) => {
     console.log(" Name:" + username + " password:" + password);
     if (username && password) {
       let login = await db.query(
-        "SELECT * FROM admin WHERE username = $1 and password = $2",
+        "SELECT * FROM public.admin WHERE username = $1 and password = $2",
         [username, hash]
       );
       res.status(200).json({
