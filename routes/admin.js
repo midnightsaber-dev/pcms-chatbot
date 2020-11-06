3dd55eec9d31c770cabebff2145b3891c2c7bc63
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-06 12:45:00 +0630

    fix merchant view page 1.7

--- routes/admin.js
+++ routes/admin.js
@@ -59,7 +59,7 @@ router.get("/merchant/view", async (req, res) => {
       res.render("admin/merchant/view_merchant", {
         title: "Merchant List | PCMS",
         place: "Merchant",
-        merchants: merchants.rows,
+        merchants: merchants,
       });
     } else {
       res.redirect("/admin/login");
