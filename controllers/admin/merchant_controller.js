6b2325f01b574642d0a11029932b9b86e0f7a990
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-06 20:58:54 +0630

    fix merchant post route 0.1

---
+++ controllers/admin/merchant_controller.js
@@ -0,0 +1,47 @@
+const db = require("../db");
+
+// Get Merchant List
+// Handle merchant update on POST.
+exports.merchant_update_post = async (req, res) => {
+  try {
+    if (req.session.loggedin) {
+      const {
+        sys_merchant_id,
+        pcms_merchant_id,
+        name,
+        email,
+        phone_number,
+        apikey,
+        orgkey,
+        address,
+        status,
+      } = req.params;
+      console.log(req.params);
+      const merchant = await db.query(
+        "UPDATE merchant SET pcms_merchant_id = $1, merchant_name = $2, merchant_email = $3, merchant_phone_number= $4, api_key = $5, org_key= $6, address= $7, status= $8 WHERE sys_merchant_id= $9 RETURNING *",
+        [
+          pcms_merchant_id,
+          name,
+          email,
+          phone_number,
+          apikey,
+          orgkey,
+          address,
+          status,
+          sys_merchant_id,
+        ]
+      );
+      if (data.rows.length > 0) {
+        res.render(
+          `/admin/merchant/detail/${merchant.rows[0].sys_merchant_id}`
+        );
+      } else {
+        res.send("database query error");
+      }
+    } else {
+      res.redirect("/admin/login");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
