c5f85561204db8647895595b4757e427dd5e9522
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-06 21:06:22 +0630

    fix merchant post route 0.2

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const db = require("../db");
 const TripleDES = require("../service/3desencrypt");
 const merchant_controller = require("../controllers/admin/merchant_controller");
