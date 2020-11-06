035e09930f83163a6497b54f75efbe6c08c5c3c6
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-06 13:21:08 +0630

    fix merchant view detail page 0.4

--- routes/admin.js
+++ routes/admin.js
@@ -143,7 +143,7 @@ router.get("/merchant/detail/:id", async function (req, res, next) {
           res.render("admin/merchant/view_merchant_detail", {
             title: "Merchant Detail | PCMS",
             place: "Merchant",
-            data: data,
+            data: data[0],
           });
         } else {
           res.send("your input is wrong." + login.rows);
