dc5f3cc5a518a4f65e42d6fb8a546b8800e74cf0
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-10 16:14:35 +0630

    fix ux event update

--- controllers/admin/event_controller.js
+++ controllers/admin/event_controller.js
@@ -97,6 +97,7 @@ exports.event_detail_get = async (req, res) => {
           "SELECT event.sys_event_id,event.pcms_event_id, event.event_name, merchant.merchant_name,event.start_date, event.end_date, event.status FROM merchant,event WHERE merchant.sys_merchant_id = (SELECT sys_merchant_id FROM event WHERE sys_event_id = $1)",
           [id]
         );
+        console.log(data.rows[0]);
         if (data.rows.length > 0) {
           res.render("admin/event/view_event_detail", {
             title: "Event Detail | PCMS",
