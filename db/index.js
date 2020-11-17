1ed7a8f6991f876b12f3e617687f1d986caca521
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-17 15:51:13 +0630

    add password reset function

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const Pool = require("pg").Pool;
 
-const cn =
-  "postgres://mzhtidpzhekzgv:0d2d621c83e1736a61ec12395684097b452a29fb31a9afd154402ee083a10028@ec2-23-20-70-32.compute-1.amazonaws.com:5432/d8460q7isklitc";
+const cn = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString: cn,
