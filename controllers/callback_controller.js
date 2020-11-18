f0cea03847c5542c2bf86e6260b6555bf9c43cc6
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-19 02:32:07 +0630

    debug  email callback function 0.2

--- controllers/callback_controller.js
+++ controllers/callback_controller.js
@@ -2,7 +2,7 @@
 exports.reset_callback_post = (req, res) => {
   try {
     console.log(req.body);
-    res.status(200);
+    res.status(200).json({ status: "success" });
   } catch (error) {
     console.log(error);
   }
