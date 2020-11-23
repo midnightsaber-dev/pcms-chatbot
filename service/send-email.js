8ed0eab5e5fc005a0f751e3a470ff76324b3f6e7
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-23 16:30:28 +0630

    update email format

--- service/send-email.js
+++ service/send-email.js
@@ -3,13 +3,18 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = (email, password) => {
-  console.log(`Here is the password in mail ${password}`);
   const msg = {
     to: email, // Change to your recipient
     from: "swteam.mpss@gmail.com", // Change to your verified sender
     subject: "Password Reset Alert",
     text: "Your password is successfully resetted.",
-    html: `<div><p><span><strong>${password}</strong></span></p></div>`,
+    html: `<div>
+    <p>Dear User,</p>
+    <p>Your password is successfully resetted. Please use the following password to login to our <a href="https://salaichitoolatt.online/admin" target="_blank" style="text-decoration:none;">portal</a>.</p>
+    </div>
+    <div style="padding:40px;margin:0 auto;">
+    <h1 align="center">${password}</h1>
+    </div>`,
   };
 
   sgMail
