67c01be3cae6b90964a6c2c098a0c0f0bf3f1bbb
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-23 18:39:29 +0630

    update mail style

--- service/send-email.js
+++ service/send-email.js
@@ -14,7 +14,20 @@ const sendMail = (email, password) => {
     </div>
     <div style="padding:40px;margin:0 auto;">
     <h1 align="center">${password}</h1>
-    </div>`,
+    </div><div>
+    <address>
+        Regards, <br>
+        MPSS Technical Team <br>
+        Room 606, Buidling 16 <br>
+        MICT Park <br>
+        Hlaing T, Yangon <br>
+        Myanmar <br>
+        <a href="tel:+959882551244" style="text-decoration: none">+959-882-551-244</a>
+    </address>
+</div>
+<footer style="background-color: #2E59D9">
+    <h4 align="center" style="color: white;">Powered by MPSS</h4>
+</footer>`,
   };
 
   sgMail
