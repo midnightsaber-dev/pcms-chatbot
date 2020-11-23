35d4d8940493c7c4abf3ddbc5052630648a15fa8
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-23 14:22:54 +0630

    update password reset function with email sending 0.1

--- service/send-email.js
+++ service/send-email.js
@@ -6,10 +6,9 @@ const sendMail = ({ email, code }) => {
   const msg = {
     to: email, // Change to your recipient
     from: "swteam.mpss@gmail.com", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text:
-      "Sending with SendGrid is Fun and easy to do anywhere, even with Node.js",
-    html: `<div><p><strong>Your password is successfully resetted. Please use the following the password to login.</strong></p><span style="padding: 5px; margin: 0 auto; border-style: solid;">${code}</span></div>`,
+    subject: "Password Reset Alert",
+    text: "Your password is successfully resetted.",
+    html: `<div><p><span><strong>${code}</strong></span></p></div>`,
   };
 
   sgMail
