a2d3d2c04e36ad65e04d50e0424b7d2bd86624e9
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-24 14:03:46 +0630

    debug hash function 1.7

--- service/encrypt.js
+++ service/encrypt.js
@@ -1,19 +1,21 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const hashPassword = async (plaintext) {
+const hashPassword = async (plaintext) => {
   try {
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(plaintext, salt);
-    const encrypted  = hash.then((value) => {return value});
+    const encrypted = hash.then((value) => {
+      return value;
+    });
     hash.catch((err) => {
       console.log(err);
-    } )
+    });
     return encrypted;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 module.exports = {
   hashPassword,
