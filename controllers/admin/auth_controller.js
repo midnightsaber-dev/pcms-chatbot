5eb7ace3e292ad724a67aceb93da2e7657b16c8a
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2020-11-26 01:54:19 +0630

    debug user login function 0.4

--- controllers/admin/auth_controller.js
+++ controllers/admin/auth_controller.js
@@ -26,7 +26,7 @@ exports.admin_login_post = async (req, res) => {
         "SELECT * FROM admin WHERE username = $1 AND password= $2",
         [username, password]
       );
-      console.log(user.rows);
+      console.log(user.rows[0]);
       console.log(comparePassword(password, user.rows[0].password));
       if (
         user.rows.length > 0 &&
