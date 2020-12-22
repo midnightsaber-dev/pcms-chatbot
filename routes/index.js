9feef200a73b84a8bdad0e7e1dfdfd0a824424f7
Shwe Yee <shweyeewin371@gmail.com>
2020-12-22 11:42:01 +0630

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/result", function(req, res, next) {
-    res.render("/partial/result");
+    res.render("/partials/result");
 })
 
 /* Reset password callback */
