a99158a4a950a6ffa1b90c67a8ca09893a5ced15
Shwe Yee <shweyeewin371@gmail.com>
2020-12-29 11:11:43 +0630

    debugging unfilled fields v6

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -4,14 +4,13 @@ exports.user_create_get = (req, res) => {
     let psid = 3288339677891256;
     console.log(req.body);
     if (
-
-        req.name,
-        req.stateNDiv,
-        req.township,
-        req.sex,
-        req.age,
-        req.phoneNo,
-        req.product,
+        req.name &&
+        req.stateNDiv &&
+        req.township &&
+        req.sex &&
+        req.age &&
+        req.phoneNo &&
+        req.product &&
         req.luckydraw
     ) {
         // let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFICT (ref_user_id) DO NOTHING", [
