3c566a419f88b11293673a3ee0693da5f18e93c6
Shwe Yee <shweyeewin371@gmail.com>
2020-12-30 13:15:37 +0630

    assign constant values

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -3,25 +3,35 @@ const db = require("../db");
 exports.user_create_get = (req, res) => {
     let psid = 3288339677891256;
     console.log(req.body);
+    const {
+        name,
+        age,
+        sex,
+        phoneNo,
+        stateNDiv,
+        township,
+        product,
+        luckydraw
+    } = req.body;
     if (
-        req.name &&
-        req.stateNDiv &&
-        req.township &&
-        req.sex &&
-        req.age &&
-        req.phoneNo &&
-        req.product &&
-        req.luckydraw
+        name &&
+        stateNDiv &&
+        township &&
+        sex &&
+        age &&
+        phoneNo &&
+        product &&
+        luckydraw
     ) {
         // let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFICT (ref_user_id) DO NOTHING", [
         let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, age, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7)", [
             psid,
-            req.name,
-            req.stateNDiv,
-            req.township,
-            req.sex,
-            req.age,
-            req.phoneNo,
+            name,
+            stateNDiv,
+            township,
+            sex,
+            age,
+            phoneNo,
 
         ], function(err, result) {
             if (err === null) {
