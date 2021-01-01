775b1c218badcc84b60df2fd45564d94d90840b1
Shwe Yee <shweyeewin371@gmail.com>
2021-01-01 12:22:32 +0630

    Update index_controller.js

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -14,6 +14,7 @@ exports.user_create_get = (req, res) => {
             product,
             luckydraw
         } = req.body;
+        console.log(name+"\n"+ age+"\n"+stateNDiv);
         if (
             name &&
             stateNDiv &&
@@ -24,8 +25,8 @@ exports.user_create_get = (req, res) => {
             product &&
             luckydraw
         ) {
-            // let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFICT (ref_user_id) DO NOTHING", [
-            let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, age, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7)", [
+            db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFICT (ref_user_id) DO NOTHING", [
+            // let fb_id = db.query("INSERT INTO user(ref_user_id, username, stateNDiv, township, sex, age, phonenumber, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7)", [
                 psid,
                 name,
                 stateNDiv,
