e432835d1fa64f49854d8b2d059e01639f74ae84
Shwe Yee <shweyeewin371@gmail.com>
2021-01-15 10:02:56 +0630

    debugging result page 7

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -26,7 +26,7 @@ exports.user_create_get = (req, res) => {
             luckydraw
         ) {
             console.log("DB Query");
-            let data =  db.query("INSERT INTO users(ref_user_id, username, region, township, sex, age, phonenumber) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (ref_user_id) DO NOTHING", [
+            let data =  db.query("INSERT INTO users(ref_user_id, username, region, township, sex, age, phonenumber) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (ref_user_id) DO NOTHING RETURNING *", [
                 psid,
                 name,
                 stateNDiv,
