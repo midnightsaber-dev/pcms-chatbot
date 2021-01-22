c93af43b08f2e3017df55c99638b0c2644b7d967
Shwe Yee <shweyeewin371@gmail.com>
2021-01-22 10:53:02 +0630

    create webhook

---
+++ routes/messenger-webhook.js
@@ -0,0 +1,55 @@
+'use strict';
+const express = require("express");
+const router = express.Router();
+const fbService = require('../service/fb-service');
+router.get("/webhook/", function(req, res) {
+    console.log("request");
+    if (
+        req.query["hub.mode"] === "subscribe" &&
+        req.query["hub.verify_token"] === process.env.FB_VERIFY_TOKEN
+    ) {
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+
+router.post("/webhook/", function(req, res) {
+    var data = req.body;
+    console.log(JSON.stringify(data));
+
+    // Make sure this is a page subscription
+    if (data.object == "page") {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    fbService.receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    fbService.receivedMessage(messagingEvent);
+                } else if (messagingEvent.delivery) {
+                    fbService.receivedDeliveryConfirmation(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    fbService.receivedPostback(messagingEvent);
+                } else if (messagingEvent.read) {
+                    fbService.receivedMessageRead(messagingEvent);
+                } else if (messagingEvent.account_linking) {
+                    fbService.receivedAccountLink(messagingEvent);
+                } else {
+                    console.log(
+                        "Webhook received unknown messagingEvent: ",
+                        messagingEvent
+                    );
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
+});
