eba28ac06435f4c1d7d873529027b474417795a9
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-04 11:41:31 +0630

    modify webhook function

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,30 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/webhook", function(req, res, next) {
-    //res.render("index", { title: "Paymal" });
-    console.log("webhook call success!");
+    // Your verify token. Should be a random string.
+    // let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN
+    console.log("verify token : "+process.env.FB_VERIFY_TOKEN);
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
 });
 
 router.post("/results", index_controller.user_create_get);
