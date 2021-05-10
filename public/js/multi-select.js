4f1d5078cb3ed2f68bfb4721685f2dab2b3911f0
mpsssoftwareteam <swteam.mpss@gmail.com>
2021-05-10 11:44:01 +0630

    Original file test!

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -1,3 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nextBtn1").addEventListener("click", () => {
+    nextPage(2);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nextBtn2").addEventListener("click", () => {
+    nextPage(3);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("prevBtn1").addEventListener("click", () => {
+    nextPage(1);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("prevBtn2").addEventListener("click", () => {
+    nextPage(2);
+  });
+});
+
+function nextPage(page) {
+  if (page == 3) {
+    document.getElementById("luckydraw-form").className = "block";
+    document.getElementById("user-info").className = "hidden";
+    document.getElementById("product").className = "hidden";
+  } else if (page == 2) {
+    document.getElementById("product").className = "block";
+    document.getElementById("luckydraw-form").className = "hidden";
+    document.getElementById("user-info").className = "hidden";
+  } else if (page == 1) {
+    document.getElementById("user-info").className = "block";
+    document.getElementById("luckydraw-form").className = "hidden";
+    document.getElementById("product").className = "hidden";
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nextBtn1").addEventListener("click", () => {
+    nextPage(2);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nextBtn2").addEventListener("click", () => {
+    nextPage(3);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("prevBtn1").addEventListener("click", () => {
+    nextPage(1);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("prevBtn2").addEventListener("click", () => {
+    nextPage(2);
+  });
+});
+
+function nextPage(page) {
+  if (page == 3) {
+    document.getElementById("luckydraw-form").className = "block";
+    document.getElementById("user-info").className = "hidden";
+    document.getElementById("product").className = "hidden";
+  } else if (page == 2) {
+    document.getElementById("product").className = "block";
+    document.getElementById("luckydraw-form").className = "hidden";
+    document.getElementById("user-info").className = "hidden";
+  } else if (page == 1) {
+    document.getElementById("user-info").className = "block";
+    document.getElementById("luckydraw-form").className = "hidden";
+    document.getElementById("product").className = "hidden";
+  }
+}
 
 function demoVisibilityone() {
   document.getElementById("one").style.display = "none";
@@ -10,11 +88,11 @@ function demoVisibilitytwoNext() {
    document.getElementById("three").style.visibility = "visible";
    document.getElementById("three").style.display = "";
 }
-function demoVisibilitytwoPre() {
+        function demoVisibilitytwoPre() {
   document.getElementById("one").style.display = "";
    document.getElementById("two").style.display = "none";
 }
-function demoVisibilitythreePre() {
+    function demoVisibilitythreePre() {
    document.getElementById("three").style.display = "none";
    document.getElementById("two").style.display = "";
 }
