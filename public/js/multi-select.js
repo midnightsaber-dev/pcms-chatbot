3d6e38dacc1cc6dce4a9a867afa50c5a235a2cd8
mpsssoftwareteam <swteam.mpss@gmail.com>
2021-05-10 10:24:16 +0630

    Add js function in multiselect.js file

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -76,3 +76,24 @@ function nextPage(page) {
     document.getElementById("product").className = "hidden";
   }
 }
+
+function demoVisibilityone() {
+  document.getElementById("one").style.display = "none";
+  document.getElementById("two").style.visibility = "visible";
+  document.getElementById("two").style.display = "";
+  
+}
+function demoVisibilitytwoNext() {
+   document.getElementById("two").style.display = "none";
+   document.getElementById("three").style.visibility = "visible";
+   document.getElementById("three").style.display = "";
+}
+        function demoVisibilitytwoPre() {
+  document.getElementById("one").style.display = "";
+   document.getElementById("two").style.display = "none";
+}
+    function demoVisibilitythreePre() {
+   document.getElementById("three").style.display = "none";
+   document.getElementById("two").style.display = "";
+}
+    
