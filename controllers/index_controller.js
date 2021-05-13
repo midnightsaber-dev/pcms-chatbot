c1d772e83b5a2f425c82f980d18ad01b2c46b08d
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-13 11:20:21 +0630

    debugging psid

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -53,6 +53,7 @@ exports.user_create_get = (req, res) => {
                     topup_amount,
                     status
                 ])
+                res.sed("data submitted!");
               }
                else {
                    
