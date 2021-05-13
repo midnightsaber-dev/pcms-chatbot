060ea2fb538d8735fde37dd92b306bc57420f46e
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-13 13:34:15 +0630

    move all webhook functions

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const callback_controller = require("../controllers/callback_controller");
 const index_controller = require("../controllers/index_controller");
+const fbService = require('../service/fb-service');
 /* GET home page. */
 router.get("/", function(req, res, next) {
     res.render("index", { title: "Paymal" });
@@ -16,5 +17,69 @@ router.post(
     "/callback/admin/password/reset/",
     callback_controller.reset_callback_post
 );
+
+
+// Adds support for GET requests to our webhook
+router.get('/webhook', (req, res) => {
+  
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
+
+router.post("/webhook/", function(req, res) {
+    var data = req.body;
+    console.log(JSON.stringify(data));
+
+    // Make sure this is a page subscription
+    if (data.object == "page") {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    fbService.receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    fbService.receivedMessage(messagingEvent);
+                } else if (messagingEvent.delivery) {
+                    fbService.receivedDeliveryConfirmation(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    fbService.receivedPostback(messagingEvent);
+                } else if (messagingEvent.read) {
+                    fbService.receivedMessageRead(messagingEvent);
+                } else if (messagingEvent.account_linking) {
+                    fbService.receivedAccountLink(messagingEvent);
+                } else {
+                    console.log(
+                        "Webhook received unknown messagingEvent: ",
+                        messagingEvent
+                    );
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
+});
 
 module.exports = router;
