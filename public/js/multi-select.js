8e5216ec18a17f9bf66b8277d137453f05a88a05
mpsssoftwareteam <swteam.mpss@gmail.com>
2021-05-13 15:22:22 +0630

    add script

---
+++ public/js/multi-select.js
@@ -0,0 +1,19 @@
+function demoVisibilityone() {
+   document.getElementById("one").style.display = "none";
+   document.getElementById("two").style.visibility = "visible";
+   document.getElementById("two").style.display = "";
+   
+ }
+       function demoVisibilitytwoNext() {
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.visibility = "visible";
+    document.getElementById("three").style.display = "";
+ }
+       function demoVisibilitytwoPre() {
+   document.getElementById("one").style.display = "";
+    document.getElementById("two").style.display = "none";
+ }
+    function demoVisibilitythreePre() {
+    document.getElementById("three").style.display = "none";
+    document.getElementById("two").style.display = "";
+ }
