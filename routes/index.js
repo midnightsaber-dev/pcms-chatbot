889174460609418f2e2f16ac47b48ccda7522f67
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-13 13:31:11 +0630

    Revert "move webhook function"
    This reverts commit ccd6b2ba289f240e28885eae052913b9308ded2a.

--- routes/index.js
+++ routes/index.js
@@ -17,31 +17,4 @@ router.post(
     callback_controller.reset_callback_post
 );
 
-// Adds support for GET requests to our webhook
-router.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-  });
-
 module.exports = router;
