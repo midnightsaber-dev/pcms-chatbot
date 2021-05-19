3a1f0c5132b61f352238c53937d256a88baabed9
mpsssoftwareteam <52270228+Eaindarey@users.noreply.github.com>
2021-05-19 13:27:40 +0630

    Test with multi slect

--- public/js/ddl.js
+++ public/js/ddl.js
@@ -571,22 +571,3 @@ function getTownship() {
   });
 }
 
-function demoVisibilityone() {
-  document.getElementById("one").style.display = "none";
-  document.getElementById("two").style.visibility = "visible";
-  document.getElementById("two").style.display = "";
-  
-}
-		function demoVisibilitytwoNext() {
-   document.getElementById("two").style.display = "none";
-   document.getElementById("three").style.visibility = "visible";
-   document.getElementById("three").style.display = "";
-}
-		function demoVisibilitytwoPre() {
-  document.getElementById("one").style.display = "";
-   document.getElementById("two").style.display = "none";
-}
-	function demoVisibilitythreePre() {
-   document.getElementById("three").style.display = "none";
-   document.getElementById("two").style.display = "";
-}
