c23a3b2e0dc92dd833a96848a0b3431aa58fcb4b
mpsssoftwareteam <52270228+Eaindarey@users.noreply.github.com>
2021-05-20 14:57:53 +0630

    alert display test

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -1,29 +1,3 @@
-$(document).ready(function(){
-    $('#nextBtn1').click(function(){
-        function demoVisibilityone() {
-            document.getElementById("one").style.display = "none";
-            document.getElementById("two").style.visibility = "visible";
-            document.getElementById("two").style.display = "";
-            
-          }
-    });
-  });
-
-  $(document).ready(function(){
-    $('#p2-PrevBtn').click(function(){
-        function demoVisibilitytwoPre() {
-            document.getElementById("one").style.display = "";
-             document.getElementById("two").style.display = "none";
-          }
-    });
-  });
-
-  $(document).ready(function(){
-    $('#P2-NextBtn').click(function(){
-        function demoVisibilitytwoNext() {
-            document.getElementById("two").style.display = "none";
-            document.getElementById("three").style.visibility = "visible";
-            document.getElementById("three").style.display = "";
-         }
-    });
-  });
+$("#nextBtn1").on("click", function(){
+    alert("clicked");
+ }); 
