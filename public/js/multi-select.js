de8fbc9d660bb26a8dfc8566b30690027181c6fd
mpsssoftwareteam <52270228+Eaindarey@users.noreply.github.com>
2021-05-20 15:03:16 +0630

    Add function in document ready function

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
     
    
     $("#nextBtn1").on("click", function(){
-       alert("clicked");
+      function demoVisibilityone() {
+  document.getElementById("one").style.display = "none";
+  document.getElementById("two").style.visibility = "visible";
+  document.getElementById("two").style.display = "";
+  
+}
     }); 
    
     $("#p2-PrevBtn").click(); //trigger event after listening to it.
