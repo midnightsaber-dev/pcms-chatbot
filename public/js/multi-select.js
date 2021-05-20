0ff80117800d76370ad9b720b43d44ca8b0f2997
mpsssoftwareteam <52270228+Eaindarey@users.noreply.github.com>
2021-05-20 14:29:32 +0630

    Add js function in multiselect.js file

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
     $('#nextBtn1').click(function(){
-       alert("HeLlo Test");
+        function demoVisibilityone() {
+            document.getElementById("one").style.display = "none";
+            document.getElementById("two").style.visibility = "visible";
+            document.getElementById("two").style.display = "";
+            
+          }
+                  function demoVisibilitytwoNext() {
+             document.getElementById("two").style.display = "none";
+             document.getElementById("three").style.visibility = "visible";
+             document.getElementById("three").style.display = "";
+          }
+                  function demoVisibilitytwoPre() {
+            document.getElementById("one").style.display = "";
+             document.getElementById("two").style.display = "none";
+          }
+              function demoVisibilitythreePre() {
+             document.getElementById("three").style.display = "none";
+             document.getElementById("two").style.display = "";
+          }
+              
     });
   });
