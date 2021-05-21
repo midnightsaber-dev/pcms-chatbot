1cb4e1dd31a634ff9cab5abfdf2e1fc4db1bd28e
mpsssoftwareteam <52270228+Eaindarey@users.noreply.github.com>
2021-05-21 14:11:33 +0630

    Update inline function

--- public/js/multi-select.js
+++ public/js/multi-select.js
@@ -28,3 +28,31 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('nextBtn1')
     .addEventListener('click', demoVisibilityone);
 });
+
+function demoVisibilitytwoNext() {
+  document.getElementById("two").style.display = "none";
+   document.getElementById("three").style.visibility = "visible";
+   document.getElementById("three").style.display = "";
+}
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('P2-NextBtn')
+    .addEventListener('click', demoVisibilitytwoNext);
+});
+
+function demoVisibilitytwoPre() {
+  document.getElementById("one").style.display = "";
+   document.getElementById("two").style.display = "none";
+}
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('p2-PrevBtn')
+    .addEventListener('click', demoVisibilitytwoPre);
+});
+
+function demoVisibilitythreePre() {
+  document.getElementById("three").style.display = "none";
+   document.getElementById("two").style.display = "";
+}
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('P3-PrevBtn')
+    .addEventListener('click', demoVisibilitythreePre);
+});
