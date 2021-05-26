00dedc39028251b4b699abcf5b1af0d06e4a6f69
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-26 11:04:03 +0630

    debugging callSendAPI 2

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -161,7 +161,26 @@ let handlePostback = (sender_psid, received_postback) => {
         }
     }
         // Send the message to acknowledge the postback
-        callSendAPI(sender_psid, response);
+        //callSendAPI(sender_psid, response);
+        let request_body = {
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
+        };
+        // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN, },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 };
 
 let callSendAPI = (sender_psid, response) => {
