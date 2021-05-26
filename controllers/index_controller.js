fcbf58ea52adf26799ec3ebf8e69dbb9d3949fc7
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-26 11:11:08 +0630

    debugging callSendAPI 3

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -134,6 +134,53 @@ let postWebhook = (req, res) => {
 
 };
 
+//Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    } else if (received_message.attachments) {
+
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+
+}
+
+// Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
 let handlePostback = (sender_psid, received_postback) => {
     console.log("Sender_psid : "+ sender_psid +", Postback : "+received_postback.payload);
     let response;
