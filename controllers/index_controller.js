4a4bd0a6314c56d90f03ade723ddbb32fa55d7d3
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-27 13:58:21 +0630

    update postWebhook function

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -117,7 +117,7 @@ let postWebhook = (req, res) => {
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-
+                handleMessage(sender_psid, webhook_event.message);
             }
 
         });
