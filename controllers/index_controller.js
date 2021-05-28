7100f6fbaabf6716e1e048d4b74a15afae17e52d
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-05-28 13:21:46 +0630

    Update index_controller.js

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -40,8 +40,8 @@ let user_create_get = (req, res) => {
                 age,
                 phoneNo
             ]);
-            console.log(user);
-            let user_id = db.query("SELECT sys_user_id FROM users WHERE ref_user_id=$1", [
+            // console.log(user);
+            let user_id = db.query("SELECT sys_user_id FROM users WHERE ref_user_id=$1 RETURNING sys_user_id", [
                 psid
             ]);
             console.log("user id :" + user_id);
