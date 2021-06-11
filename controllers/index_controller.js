5dc47298b934338fabf60e5e06bf1fa8a4a9dcf5
Lukia848 <60330180+Lukia848@users.noreply.github.com>
2021-06-11 11:58:53 +0630

    debug user_create_get

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -39,7 +39,7 @@ let user_create_get = async (req, res) => {
                 age,
                 phoneNo
             ]);            
-            data = data.rows[0].ref_user_id;
+            data = data.rows[0].values;
             console.log("data :"+ data);
             if(!(data === null)){
                 // res.render("result.ejs",{ title : "result"});
